5fbf2fc94279e1af8cde3eb884617f33409f26d5
Qing An <qa4@rice.edu>
2026-05-04 11:52:49 -0700

    Fix comments and clean codes

--- verl/workers/rollout/replica.py
+++ verl/workers/rollout/replica.py
@@ -378,14 +378,7 @@ def _load_vllm_omni():
     return vLLMOmniReplica
 
 
-def _load_vllm_omni_ar():
-    from verl_omni.workers.rollout.vllm_rollout.vllm_omni_async_server import vLLMOmniARReplica
-
-    return vLLMOmniARReplica
-
-
 RolloutReplicaRegistry.register("vllm_omni", _load_vllm_omni)
-RolloutReplicaRegistry.register("vllm_omni_ar", _load_vllm_omni_ar)
 
 
 def get_rollout_replica_class(rollout: str, disaggregation_enabled: bool = False) -> type[RolloutReplica]:
